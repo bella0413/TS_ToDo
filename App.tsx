8ac3e9f66946d55477fcaf903d2f0208102ac3ca
YoonJeon <yoonjeon0413@gmail.com>
2022-07-30 15:13:40 +0900

    Make input form

--- App.tsx
+++ App.tsx
@@ -1,117 +1,64 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
-import React, {type PropsWithChildren} from 'react';
+import React from 'react';
 import {
   SafeAreaView,
   ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
+  TextInput,
+  TouchableHighlight,
   View,
 } from 'react-native';
+import Title from './src/components/Title';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section: React.FC<
-  PropsWithChildren<{
-    title: string;
-  }>
-> = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+const App: React.FC = () => {
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+    <SafeAreaView>
+      <ScrollView contentContainerStyle={styles.container}>
+        <Title></Title>
+        <View style={styles.form}>
+          <TextInput style={styles.inputBox}></TextInput>
+          <TouchableHighlight style={styles.inputBtn} activeOpacity={0.8} underlayColor={'#CBE2E3'} onPress={()=>{console.log('press')}}>
+            <Text style={styles.inputBtnText}>입력</Text>
+          </TouchableHighlight>
         </View>
-      </ScrollView>
+     </ScrollView>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    height: '100%',
+    alignItems: 'center',
+    paddingHorizontal: 27
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  inputBox: {
+    borderWidth: 1,
+    borderColor: '#999999',
+    borderTopLeftRadius: 10,
+    borderBottomLeftRadius: 10,
+    width: '80%',
+    height: 44,
+    paddingLeft: 15
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  inputBtn: {
+    borderWidth: 1,
+    borderLeftWidth: 0,
+    borderColor: '#6B7B7C',
+    borderTopRightRadius: 10,
+    borderBottomRightRadius: 10,
+    width: '20%',
+    height: 44,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  highlight: {
-    fontWeight: '700',
+  inputBtnText: {
+    color: '#5B6B6C'
   },
+  form: {
+    flexDirection: 'row'
+  }
 });
 
 export default App;
